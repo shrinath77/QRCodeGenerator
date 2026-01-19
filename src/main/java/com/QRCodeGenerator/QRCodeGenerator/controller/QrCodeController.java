8413cd0817a940ecb8f3648f5cef280f0975db7b
shrinath.kadam <kadamshrinath7777@gmail.com>
2026-01-19 22:19:48 +0530

    Addded

--- src/main/java/com/QRCodeGenerator/QRCodeGenerator/controller/QrCodeController.java
+++ src/main/java/com/QRCodeGenerator/QRCodeGenerator/controller/QrCodeController.java
@@ -15,7 +15,7 @@ public class QrCodeController {
 
     private final QrCodeService qrCodeService;
 
-    public QrCodeController(QrCodeService qrCodeService) {
+    public QrCodeController(QrCodeSegit rvice qrCodeService) {
 
         this.qrCodeService = qrCodeService;
     }
