e4892833eb9c5ae1cbe62f928e0c868ab975448a
shrinath.kadam <kadamshrinath7777@gmail.com>
2026-01-19 11:28:08 +0530

    War

--- src/main/java/com/QRCodeGenerator/QRCodeGenerator/QrCodeGeneratorApplication.java
+++ src/main/java/com/QRCodeGenerator/QRCodeGenerator/QrCodeGeneratorApplication.java
@@ -2,9 +2,15 @@ package com.QRCodeGenerator.QRCodeGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QrCodeGeneratorApplication {
+public class QrCodeGeneratorApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return  builder.sources(QrCodeGeneratorApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QrCodeGeneratorApplication.class, args);
