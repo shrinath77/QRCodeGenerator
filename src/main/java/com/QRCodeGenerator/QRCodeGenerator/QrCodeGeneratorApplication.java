ad275b615053c717fec5c44f6d6fd7bcbb2be52b
shrinath.kadam <kadamshrinath7777@gmail.com>
2026-01-19 12:36:52 +0530

    Added spring Actuators

--- src/main/java/com/QRCodeGenerator/QRCodeGenerator/QrCodeGeneratorApplication.java
+++ src/main/java/com/QRCodeGenerator/QRCodeGenerator/QrCodeGeneratorApplication.java
@@ -2,18 +2,13 @@ package com.QRCodeGenerator.QRCodeGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QrCodeGeneratorApplication extends SpringBootServletInitializer {
+public class QrCodeGeneratorApplication {
 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return  builder.sources(QrCodeGeneratorApplication.class);
-	}
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QrCodeGeneratorApplication.class, args);
-	}
+        SpringApplication.run(QrCodeGeneratorApplication.class, args);
+    }
 
 }
