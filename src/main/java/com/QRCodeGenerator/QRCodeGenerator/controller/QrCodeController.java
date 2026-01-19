4c5fdedd6eeb7b311c959bffec1c665a363d6333
shrinath.kadam <kadamshrinath7777@gmail.com>
2026-01-19 17:48:20 +0530

    Added Logger Lib

--- src/main/java/com/QRCodeGenerator/QRCodeGenerator/controller/QrCodeController.java
+++ src/main/java/com/QRCodeGenerator/QRCodeGenerator/controller/QrCodeController.java
@@ -1,15 +1,17 @@
 package com.QRCodeGenerator.QRCodeGenerator.controller;
-
 import com.QRCodeGenerator.QRCodeGenerator.service.QrCodeService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/qr")
 public class QrCodeController {
+
+    private static final Logger log = LoggerFactory.getLogger(QrCodeController.class);
 
     private final QrCodeService qrCodeService;
 
@@ -21,13 +23,17 @@ public class QrCodeController {
     @GetMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<byte[]> generateQr(@RequestParam String text) throws Exception {
 
+        log.info("Genrating QR For "+text);
         byte[] qrImage = qrCodeService.generateQrCode(text);
         return ResponseEntity.ok(qrImage);
     }
+
     @GetMapping("/test")
     public ResponseEntity<Map<String, String>> test() {
+
+        log.warn("Test Api called");
+
         return ResponseEntity.ok(Map.of("status", "ok"));
     }
-
 
 }
